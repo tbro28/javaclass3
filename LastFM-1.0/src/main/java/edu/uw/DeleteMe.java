bee7af7726936e18e8b2772d2c87c85437956f8e
Tim Brown <brownti28@yahoo.com>
2021-04-12 18:38:23 -0700

    first commit

---
+++ LastFM-1.0/src/main/java/edu/uw/DeleteMe.java
@@ -0,0 +1,10 @@
+package edu.uw;
+
+/**
+ * Place holder class to aid in importing template maven project into Eclipse, servers no other purpose.
+ *
+ * @author Russ Moul
+ */
+public final class DeleteMe {
+}
+
